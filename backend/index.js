f287bea7befeac81658bd45e3ba3f08c9b2e4123
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-01-02 16:44:31 -0500

    Javascript FullStack #3 , Express Middlewares.

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
 // Inicializations
 const app = express();
 
 // Settings
 app.set('port', 3000);
+
+// Middlewares
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename(req, file, cb) {
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+    }
+});
+app.use(multer({storage}).single('image'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 // Start the server
 app.listen(app.get('port'), () => {
