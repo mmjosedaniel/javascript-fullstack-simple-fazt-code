1b1f75c75974e22f2eaff4e83f69f233f82333cf
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-01-06 14:16:51 -0500

    Javascript FullStack #10 , User Interface & Bootstrap 4

--- backend/public/js/bundle.js
+++
@@ -1,2 +0,0 @@
-(()=>{var e={416:(e,r,t)=>{"use strict";t.r(r)}},r={};function t(o){if(r[o])return r[o].exports;var n=r[o]={exports:{}};return e[o](n,n.exports,t),n.exports}t.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},alert("It works"),t(416)})();
-//# sourceMappingURL=bundle.js.map
